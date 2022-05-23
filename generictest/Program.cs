a811c5bae94f4ffbf8094ccfb973eeb0a0a34a1b
8778saquib <saquib.nawaz009@gmail.com>
2022-05-23 23:08:09 +0530

    GenericsTestMaximum

---
+++ generictest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generictest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Find Maximum Problem using Generics");
+        }
+    }
+}
