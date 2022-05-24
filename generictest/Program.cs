d218b976ec0b2828ccd7791164b65228c596391c
8778saquib <saquib.nawaz009@gmail.com>
2022-05-24 20:24:39 +0530

    max among float

--- generictest/Program.cs
+++ generictest/Program.cs
@@ -8,6 +8,7 @@ namespace generictest
         {
             Console.WriteLine("welcome to Find Maximum Problem using Generics");
             Console.WriteLine("\n1. Find maximum among three integers");
+            Console.WriteLine("2. Find maximum among three floats");
             Console.Write("Enter your choice : ");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -16,7 +17,7 @@ namespace generictest
                     MaxAmongThree maxAmongThree = new MaxAmongThree();
 
                     //Test case 1 : Max Number in First position
-                    Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(3, 4, 1));
+                    Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(3, 2, 1));
 
                     //Test case 2 : Max Number in Second position
                     Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(1, 3, 2));
@@ -24,6 +25,20 @@ namespace generictest
                     //Test case 3 : Max Number in Third position
                     Console.WriteLine("Maximum integer is : " + maxAmongThree.MaxAmongThreeInt(1, 2, 3));
                     break;
+
+                case 2:
+                    MaxAmongThree maxAmongThree1 = new MaxAmongThree();
+
+                    //Test case 1 : Max Number in First position
+                    Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(3.1f, 2.1f, 1.1f));
+
+                    //Test case 2 : Max Number in Second position
+                    Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(1.1f, 3.1f, 2.1f));
+
+                    //Test case 3 : Max Number in Third position
+                    Console.WriteLine("Maximum float number is : " + maxAmongThree1.MaxAmongThreeFloat(1.1f, 2.1f, 3.1f));
+                    break;
+
                 default:
                     Console.WriteLine("Please choose correct option from the list");
                     break;
