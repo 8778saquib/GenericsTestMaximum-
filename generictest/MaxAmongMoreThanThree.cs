3a3ef67fe8f56650e7519b9c22710ecbb1094f66
8778saquib <saquib.nawaz009@gmail.com>
2022-05-27 20:59:46 +0530

    MoreThanThreeParameter

---
+++ generictest/MaxAmongMoreThanThree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace generictest
+{
+    public class MaxAmongMoreThanThree<T> where T : IComparable
+    {
+        public T[] values;
+
+        public MaxAmongMoreThanThree(T[] values)
+        {
+            this.values = values;
+        }
+
+        public T[] SortValues(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        public T FindMax(T[] values)
+        {
+            //Storing max value in maxValue variable
+            //this keyword used for accessing instance/class level variable
+            var sortedValues = SortValues(this.values);
+            return sortedValues[sortedValues.Length - 1];
+        }
+
+        public T FindMaxMethod()
+        {
+            var maxValue = FindMax(this.values);
+            return maxValue;
+        }
+    }
+}
