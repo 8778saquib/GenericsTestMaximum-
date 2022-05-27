120998deab5bab80613fc10141856b0d36908e2b
8778saquib <saquib.nawaz009@gmail.com>
2022-05-27 19:23:59 +0530

    genericsmethod

--- generictest/Program.cs
+++ generictest/Program.cs
@@ -10,6 +10,7 @@ namespace generictest
             Console.WriteLine("\n1. Find maximum among three integers");
             Console.WriteLine("2. Find maximum among three floats");
             Console.WriteLine("3. Find maximum among three strings");
+            Console.WriteLine("4. Find maximum among int, float, string using generic method");
             Console.Write("Enter your choice : ");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -51,6 +52,24 @@ namespace generictest
 
                     //Test case 3 : Max String in Third position
                     Console.WriteLine("Maximum string is : " + maxAmongThree2.MaxAmongThreeString("1", "2", "3"));
+                    break;
+                case 4:
+                    MaxAmongThreeGeneric maxAmongThreeGeneric = new MaxAmongThreeGeneric();
+
+                    //Test case for integer
+                    Console.WriteLine("Maximum integer is : " + maxAmongThreeGeneric.MaxAmongThree(3, 2, 1));
+                    Console.WriteLine("Maximum integer is : " + maxAmongThreeGeneric.MaxAmongThree(1, 3, 2));
+                    Console.WriteLine("Maximum integer is : " + maxAmongThreeGeneric.MaxAmongThree(1, 2, 3));
+
+                    //Test case for float
+                    Console.WriteLine("Maximum float number is : " + maxAmongThreeGeneric.MaxAmongThree(3.1f, 2.1f, 1.1f));
+                    Console.WriteLine("Maximum float number is : " + maxAmongThreeGeneric.MaxAmongThree(1.1f, 3.1f, 2.1f));
+                    Console.WriteLine("Maximum float number is : " + maxAmongThreeGeneric.MaxAmongThree(1.1f, 2.1f, 3.1f));
+
+                    //Test case 1 : Max String in First position
+                    Console.WriteLine("Maximum string is : " + maxAmongThreeGeneric.MaxAmongThree("3", "2", "1"));
+                    Console.WriteLine("Maximum string is : " + maxAmongThreeGeneric.MaxAmongThree("1", "3", "2"));
+                    Console.WriteLine("Maximum string is : " + maxAmongThreeGeneric.MaxAmongThree("1", "2", "3"));
                     break;
 
                 default:
